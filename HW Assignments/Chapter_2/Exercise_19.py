4a9ff003728d87b7b01594f7493144f290341aca
noel_maldonado <noel450@gmail.com>
2020-09-26 23:55:43 -0400

    update

---
+++ HW Assignments/Chapter_2/Exercise_19.py
@@ -0,0 +1,10 @@
+#(Financial application: calculate future investment value) Write a program that
+#reads in an investment amount, the annual interest rate, and the number of years,
+#and displays the future investment value using the following formula:
+#
+#futureInvestmentValue = investmentAmount * (1 + monthlyInterestRate)^numberOfMonths
+#
+#For example, if you enter the amount 1000, an annual interest rate of 4.25%,
+#and the number of years as 1, the future investment value is 1043.33. Here is a
+#sample run:
+
---
+++ HW Assignments/Chapter_2/Exercise_26.py
@@ -0,0 +1,3 @@
+#(Turtle: draw a circle) Write a program that prompts the user to enter the
+#center and radius of a circle, and then displays the circle and its area, as shown
+#in Figure 2.5.
